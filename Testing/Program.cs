61425233c6d4629823be03d9e6fce0ec8336ced5
Jacob Bes Tarun <jacobtarun782@outlook.com>
2020-08-07 16:27:56 +0800

    Add project for testing.

---
+++ Testing/Program.cs
@@ -0,0 +1,45 @@
+﻿using RaidyFileUtils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file;
+
+            Console.Write("Path to file: ");
+            file = Console.ReadLine().Trim('\"');
+
+            if (file != string.Empty)
+            {
+                RaidyReader reader = new RaidyReader(new FileStream(file, FileMode.Open, FileAccess.Read));
+
+                string outputDirectory = file.Substring(file.LastIndexOf('\\') + 1);
+                Directory.CreateDirectory(outputDirectory);
+
+                foreach (var item in reader.FileEntries)
+                {
+                    using (var fileStream = new FileStream(outputDirectory + "\\" + item.fileName, FileMode.Create, FileAccess.Write))
+                    {
+                        using (var memoryStream = reader.GetFile(item))
+                        {
+                            memoryStream.CopyTo(fileStream);
+                        }
+                    }
+
+                    Console.WriteLine("Extracted " + item.fileName);
+                }
+            }
+
+            Console.WriteLine("Done.");
+            Console.ReadKey();
+        }
+    }
+}
